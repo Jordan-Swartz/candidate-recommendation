263a8752d5030d48dc336f807ccc1a1099cde14b
Jordan Swartz <JDSwartz@proton.me>
2025-08-07 23:10:21 -0700

    Implemented UI in app.py and added upload job description and upload resume functionality.

--- app.py
+++ app.py
@@ -1,26 +1,94 @@
 """
-    This class contains the UI layout
+    This class contains the UI layout for the application
 """
 import streamlit as st
-
-st.title("Candidate Recommendation Engine ⚙️")
+from candidate_engine.resume_parser import parse_resumes
+from candidate_engine.utils import *
+from candidate_engine.applicant import Applicant
 
 # collect resume & name
 # calculate cosine similarity
 # create applicant object
 # sort list
 
+# Sidebar
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
 
-st.write()
-# st.write("Most objects") # df, err, func, keras!
-# st.write(["st", "is <", 3])
-#
-# st.text("Fixed width text")
-# st.markdown("_Markdown_")
-# st.latex(r""" e^{i\pi} + 1 = 0 """)
-# st.title("My title")
-# st.header("My header")
-# st.subheader("My sub")
-# st.code("for i in range(8): foo()")
-# st.badge("New")
-# st.html("<p>Hi!</p>")
+# Header & Info
+vertical_alignment = st.selectbox(
+    "Vertical alignment", ["top", "center", "bottom"], index=2
+)
+
+with st.container():
+    st.markdown(
+        """
+        <div style="
+            background-color: #2196f3;
+            border-radius: 15px;
+            margin-bottom: 1rem;
+            color: white;
+            text-align: center;
+        ">
+            <h1 style="margin-bottom: 0.3em; font-size: 3rem;">
+                Candidate Recommendation Engine
+            </h1>
+            <h3 style='font-style: italic; font-weight: normal;'>
+                Find The Best Candidate For The Job!
+            </h3>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+
+# How to info
+st.subheader("How to use")
+# row 1
+col_icon, col_text = st.columns([1, 8])
+with col_icon:
+    st.image("resources/upload.png", width=48)
+with col_text:
+    st.markdown("Get started by entering your desired job description into the provided text field. Then, upload your candidate resumes either by pasting the text directly or by uploading files—whichever works best for you!")
+st.divider()
+
+# row 2
+col_icon, col_text = st.columns([1, 8])
+with col_icon:
+    st.image("resources/brain.png", width=48)
+with col_text:
+    st.markdown("Allow our engine to do the heavy lifting by analyzing the job description and resumes using advanced natural language processing. It will compute the cosine similarity between the job requirements and each resume to identify the most relevant candidates!")
+st.divider()
+
+# row 3
+col_icon, col_text = st.columns([1, 8])
+with col_icon:
+    st.image("resources/bulb.png", width=52)
+with col_text:
+    st.markdown("After processing, enjoy a ranked list of candidates based on how well they match the job description! Our engine provides a detailed breakdown of each match, helping you understand exactly why a candidate received their ranking.")
+
+st.markdown(
+            "<h4 style='text-align: center;'>Get Started Below!</h4>",
+            unsafe_allow_html=True
+        )
+st.divider()
+
+# Collect Input
+col1, col2 = st.columns(2)
+
+with col1:
+    st.subheader("Enter Job Description")
+    job_description = st.text_area("Job Description", height=160)
+
+with col2:
+    st.subheader("Upload Resume Files")
+    text_toggle_state = st.session_state.get('text_toggle_state')
+
+    if text_toggle_state:
+        uploaded_resume_text = st.text_area("Resume Input", height=160)
+    else:
+        uploaded_resumes = st.file_uploader("", accept_multiple_files=True)
+
+    text_toggle = st.toggle('Text Input', value=text_toggle_state, key="text_toggle_state")
+
